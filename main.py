363ef182a613c583ea41751f791b2faa41764cc0
Adrien Bérard <adrienberard@hotmail.fr>
2023-09-20 16:11:05 +0200

    ajout du choix par date

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import sys
-from PyQt5 import QtWidgets
+from PyQt5 import QtWidgets, QtCore
 from PyQt5.QtWidgets import QFileDialog
-from PyQt5.QtWidgets import QApplication
+from PyQt5.QtWidgets import QApplication, QFileDialog, QMessageBox, QInputDialog, QProgressBar
 from PyQt5.QtCore import QTimer
 import configparser
 import os
@@ -9,6 +9,28 @@ import pandas as pd
 from openpyxl import load_workbook
 import subprocess
 from functions_scrapping import ZLME_scrap, EURX_scrap
+import locale
+from datetime import datetime, timedelta, date
+from dateutil.easter import easter
+import datetime
+
+
+locale.setlocale(locale.LC_ALL, 'fr_FR.UTF-8')
+now = datetime.datetime.now().date()
+yesterday = now - timedelta(days=1)
+
+# Ajd 'vendredi'
+day_of_week = now.strftime("%A")
+
+# Hier '01/06/2023
+date_yesterday = yesterday.strftime("%d/%m/%Y")
+
+# Hier 'jeudi'
+yesterday_day_of_week = yesterday.strftime("%A")
+
+# Hier 'jeudi 01 juin 2023
+yesterday_holiday = yesterday.strftime("%A %d %B")
+
 
 # Lire le chemin du fichier à partir du fichier config.ini
 script_dir = os.path.dirname(os.path.abspath(__file__))
@@ -23,14 +45,18 @@ else:
 class MyApp(QtWidgets.QWidget):
     def __init__(self):
         super().__init__()
-
-        self.startup_timer = QTimer()
-        self.startup_timer.singleShot(0, self.run_script)
-
-        self.shutdown_timer = QTimer()
-        self.shutdown_timer.singleShot(60000, self.stop_script)
         
         self.initUI()
+
+        self.config = configparser.ConfigParser()
+        self.config.read('config.ini')
+        auto_start = self.config.getboolean('SETTINGS', 'auto_start', fallback=False)
+        self.param1_checkbox.setChecked(auto_start)
+
+        if auto_start:
+            self.run_script()
+            QtCore.QTimer.singleShot(120000, self.stop_script)
+
         
     def initUI(self):
         # Création de l'interface
@@ -49,6 +75,23 @@ class MyApp(QtWidgets.QWidget):
         button_layout.addWidget(self.modify_button)
         button_layout.addWidget(self.open_button)
         layout.addLayout(button_layout)
+
+        self.start_date_edit = QtWidgets.QDateEdit(datetime.datetime.now().date())
+        self.end_date_edit = QtWidgets.QDateEdit(datetime.datetime.now().date())
+
+        # Champs choix des dates
+        self.start_date_edit.setCalendarPopup(True)
+        self.end_date_edit.setCalendarPopup(True)
+
+        layout.addWidget(QtWidgets.QLabel("Date de début :"))
+        layout.addWidget(self.start_date_edit)
+        layout.addWidget(QtWidgets.QLabel("Date de fin :"))
+        layout.addWidget(self.end_date_edit)
+
+        # Créez l'objet QCheckBox avant de l'ajouter au layout
+        self.use_date_range_checkbox = QtWidgets.QCheckBox("Utiliser la plage de dates")
+        layout.addWidget(self.use_date_range_checkbox)
+
         
         # Logger
         self.logger = QtWidgets.QTextEdit()
@@ -64,6 +107,19 @@ class MyApp(QtWidgets.QWidget):
         self.run_button.clicked.connect(self.run_script)
         
         self.setLayout(layout)
+
+        # Création de la section Paramètres
+        self.settings_group = QtWidgets.QGroupBox("Paramètres")
+        settings_layout = QtWidgets.QVBoxLayout()
+        # Ajout de différents widgets pour les paramètres
+        self.param1_checkbox = QtWidgets.QCheckBox("Lancer le script automatiquement au démarrage de l'application.")
+        self.param1_checkbox.stateChanged.connect(self.saveSettings)
+        # Ajout des widgets au layout des paramètres
+        settings_layout.addWidget(self.param1_checkbox)
+        # Définition du layout des paramètres comme layout du QGroupBox
+        self.settings_group.setLayout(settings_layout)
+        # Ajout du QGroupBox au layout principal
+        layout.addWidget(self.settings_group)
         
         # Paramètres de la fenêtre
         self.setWindowTitle('Prices USD/EUR')
@@ -88,36 +144,67 @@ class MyApp(QtWidgets.QWidget):
             self.log('Fichier ouvert.')
         except subprocess.CalledProcessError as e:
             self.log('Fichier non trouvé.')
-            
+    
+    def saveSettings(self):
+        self.config.read('config.ini')
+    
+        # Mettez à jour seulement la clé spécifique
+        if not self.config.has_section('SETTINGS'):
+            self.config.add_section('SETTINGS')
+        self.config.set('SETTINGS', 'auto_start', str(self.param1_checkbox.isChecked()))
+        
+        # Écrivez le fichier de configuration mis à jour
+        with open('config.ini', 'w') as configfile:
+            self.config.write(configfile)
 
     def run_script(self):
+
         # Fonction pour lancer le script
         self.log('Script lancé.')
+        # Vérifiez l'état de la checkbox pour déterminer les dates à utiliser
+        if self.use_date_range_checkbox.isChecked():
+            start_date = self.start_date_edit.date().toPyDate()
+            end_date = self.end_date_edit.date().toPyDate()
+        else:
+            start_date = end_date = datetime.date.today()
+        
+        # #  # # # # ZLME # # # # # # 
+
         try:
-            result_ZLME = ZLME_scrap()
-            if len(result_ZLME) == 2:
-                date_ZLME, data_ZLME = result_ZLME
-            else:
+            result_ZLME = ZLME_scrap(start_date, end_date)
+            
+            # Vérifiez si une erreur s'est produite en vérifiant si le résultat est une chaîne
+            if isinstance(result_ZLME, str):
                 self.log(f"Erreur lors du scrapping ZLME : {result_ZLME}")
                 return
+            
+            # Log chaque entrée dans result_ZLME
+            for date_ZLME, data_ZLME in result_ZLME:
+                self.log(f"ZLME = {date_ZLME} : {data_ZLME}")
         except Exception as e:
             self.log(f"Erreur lors du scrapping ZLME : {e}")
             return
+        
+        # # # # # # EURX # # # # # # #
 
         try:
-            result_EURX = EURX_scrap()
-            if len(result_EURX) == 2:
-                date_EURX, data_EURX= result_EURX
-            else:
+            result_EURX = EURX_scrap(start_date, end_date)
+            
+            # Vérifiez si une erreur s'est produite en vérifiant si le résultat est une chaîne
+            if isinstance(result_EURX, str):
                 self.log(f"Erreur lors du scrapping EURX : {result_EURX}")
-
+                return
+            
+            # Log chaque entrée dans result_EURX
+            for date_EURX, data_EURX in result_EURX:
+                self.log(f"EURX = {date_EURX} : {data_EURX}")
         except Exception as e:
-            self.log(f'Erreur lors du scrapping EURX : {e}')
+            self.log(f"Erreur lors du scrapping EURX : {e}")
             return
 
         # Logger les données dans l'interface PyQt
-        self.log(f"Date: {date_ZLME}, Valeur: {data_ZLME}")
-        self.log(f"Date: {date_EURX}, Valeur: {data_EURX}")
+        # self.log(f"Date: {date_ZLME}, Valeur: {data_ZLME}")
+        # self.log(f"Date: {date_EURX}, Valeur: {data_EURX}")
 
         # Chemin vers le fichier Excel
         excel_path = self.path_edit.text()
@@ -138,7 +225,7 @@ class MyApp(QtWidgets.QWidget):
             sheet = book['ZLME']
 
             # Trouver la dernière ligne et ajouter les nouvelles données
-            new_row = ['ZLME', date_ZLME, data_ZLME, 'USD', 'EUR']
+            new_row = ['ZLME', date, value, 'USD', 'EUR']
             
             # Trouver la dernière ligne disponible et ajouter la nouvelle ligne
             sheet.append(new_row)
